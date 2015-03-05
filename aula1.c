054fe31ecd20b8c3e5f47fcb9d9c729a21978185
Vinicius Rossato Bitencourt <vrossato@inf.ufsm.com>
2015-03-05 09:17:36 -0300

    adicionei

--- aula1.c
+++ aula1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main (void) {
-	printf ("hello world");
+void main (void) {
+	
+	FILE* f;
+	int matricula;
+	char nome [100];
+
+	printf ("informe a matricula");
+	scanf ("%d", &matricula);
+	
+	printf ("nome");
+	scanf("%s", nome);
+	f = fopen ("saida.txt", "w");
+	fprintf(f, "%d %s\n", matricula, nome);
+	fclose(f);
 }
